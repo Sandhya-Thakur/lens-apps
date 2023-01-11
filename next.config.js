136100a4dd1dbcc6db7bbd5f1d92da048a4f2b7d
sandhya-thakur <thaku.sandhya@gmail.com>
2023-01-11 22:06:35 +0530

    Message

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
   images: {
     domains: [
       'ipfs.infura.io',
       'statics-polygon-lens-staging.s3.eu-west-1.amazonaws.com',
       'lens.infura-ipfs.io',
+      'source.unsplash.com',
       ""
     ],
   },
